99a821d315e626f72b84e22af2acc4b2c8b21564
05AliAhmed <20ali0504@gmail.com>
2026-03-11 02:41:57 +0800

    MainMenu
    Play quit and Back buttons done

---
+++ Assets/Scripts/Lvl1_Buttons.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lvl1_Buttons : MonoBehaviour
+{
+    public void BackOption()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUit is working");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
